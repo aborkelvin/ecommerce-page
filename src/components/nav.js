9d811ff87a131d3c3d87818478f2b16291e77d5e
Kelvin <aborkelvin@gmail.com>
2022-07-19 20:57:47 +0100

    adding mobile/tab designs for nav bar

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,12 +2,16 @@ import { useState,useEffect } from 'react';
 
 import logo from '../images/logo.svg';
 import cartimg from '../images/icon-cart.svg';
-import avatar from '../images/image-avatar.png'
+import avatar from '../images/image-avatar.png';
+import menuicon from '../images/icon-menu.svg'
+import closebar from '../images/icon-close.svg'
 import './nav.css';
 import ShoppingCart from './shoppingcart';
 import { Link } from 'react-router-dom';
 
 function Nav(props){
+    
+    const[shownav,setshownav] = useState(false);
     
     function showcart() {
         //if set to true, the showcart class is added to the shopping cart div making the display block
@@ -18,19 +22,36 @@ function Nav(props){
         }
     }
 
+    function openmenu(){
+        if(shownav == false){
+            setshownav(true);
+        }
+    }
+
+    function closemenu(){
+        if(shownav == true){
+            setshownav(false);
+        }
+    }
+
+    
+
+
     return(
-        <div className='nav-container'>            
+        <div className='nav-container'>
+            <img src = {menuicon} className = 'menuicon cursor' onClick = {openmenu} />            
             <img src={logo} className='logo' />
-            <nav>
+            <nav className = {`headnav ${shownav?"shownav":''}`} >
                 <ul>
+                    <img src = {closebar} className = 'closebar cursor' onClick = {closemenu} />
                     <Link to = '/ecommerce-page'>
                         <li>Shop</li> 
                     </Link>
                     <Link to = '/ecommerce-page/shop'>
                         <li>Collections</li>
-                    </Link>
-                    <li><a href='#'>Women</a></li>
-                    <li><a href='#'>Men</a></li>
+                    </Link>                                        
+                    <li>Women </li>                    
+                    <li>Men</li>
                 </ul>
             </nav>
             <img src = {cartimg} className = 'cartimg cursor' onClick = {showcart} />
